7656c3bbd62f41f9f807cae5865551cb45105ed7
Demian <53890358+demian35@users.noreply.github.com>
2023-01-24 20:52:31 -0600

    programa ya listo para usarse y su ejectuable para sistemas windows

--- convierteImagen.py
+++ convierteImagen.py
@@ -1,9 +1,8 @@
 import cv2
 
 
-filename= r"C:\Users\josed\OneDrive\Imágenes\IMG_3110.JPG"
+imagen= cv2.imread("inserte su imagen (el nombre del archivo)")
 #le pasamos la imagen con la que trabajaremos
-imagen=cv2.imread(filename)
 
 #aplicamos filtro a la foto , en ese caso usamos la funcion cvtcolor y le pasamos el color gris de cv2
 filtrogrises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
@@ -20,4 +19,4 @@ invertblur=cv2.bitwise_not(blurfilter)
 pencilsketch=cv2.divide(filtrogrises,invertblur,scale=256.0)
 
 #regresamos la imagen pintada a lapiz
-cv2.imwrite("output.jpg",pencilsketch)
+cv2.imwrite("output.png",pencilsketch)
---
+++ ejecutable.bat
@@ -0,0 +1,11 @@
+echo BIENVENIDO %USERNAME% A TU JUEGO CLASICO DE LA SERPIENTE DEL NOKIA
+echo.%TIME% %DATE%
+echo ejecutando juegoSnake
+echo.
+
+::RUTA DE EJECUCION
+    ::Cambiar por la ruta donde se encuentre o guarde usted el directorio del juego
+    cd C:\Users\josed\OneDrive\Documentos\ProyectosdeOcio\pythonSketch
+
+    ::Comando para correr el juego
+    python3 convierteImagen.py
