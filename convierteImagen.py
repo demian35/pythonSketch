d045bfd6cea8ba97b4352e83f2d66e843e0e610f
Demian <53890358+demian35@users.noreply.github.com>
2023-01-24 19:59:27 -0600

    a grices lo invertimos y a la vez al invertido le aplicamos blur para tambien invertirlo

--- convierteImagen.py
+++ convierteImagen.py
@@ -7,3 +7,11 @@ imagen=cv2.imread(filename)
 
 #aplicamos filtro a la foto , en ese caso usamos la funcion cvtcolor y le pasamos el color gris de cv2
 filtrogrises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+#invertimos el flitro de grises
+invert=cv2.bitwise_not(filtrogrises)
+
+#aplicamos el fitro blur al invert
+blurfilter=cv2.GaussianBlur(invert,(21,21),0)
+
+#invertimos a blur
+invertblur=cv2.bitwise_not(blurfilter)
