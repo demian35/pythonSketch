3a3c877f91bac3cf3d56bb44c158ecc8a7c31cb7
Demian <53890358+demian35@users.noreply.github.com>
2023-01-25 17:39:00 -0600

    agregue un pequeño menu para seleccionar el filtro deseado

--- convierteImagen.py
+++ convierteImagen.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-imagen= cv2.imread("inserte su imagen (el nombre del archivo)")
+imagen= cv2.imread("inserte una imagen")
 #le pasamos la imagen con la que trabajaremos
 
 #aplicamos filtro a la foto , en ese caso usamos la funcion cvtcolor y le pasamos el color gris de cv2
@@ -18,5 +18,20 @@ invertblur=cv2.bitwise_not(blurfilter)
 #aplicamos el pencil sketch
 pencilsketch=cv2.divide(filtrogrises,invertblur,scale=256.0)
 
+
+
+
+print("Que filtro desea usar : seleccione una opcion 1 pencilSketch , 2 blur , 3 grises , 4 salir")
+opcion=int(input())
 #regresamos la imagen pintada a lapiz
-cv2.imwrite("output.png",pencilsketch)
+
+match opcion:
+    case 1:
+        cv2.imwrite("outputgrey.jpg", filtrogrises)
+    case 2:
+        cv2.imwrite("outputblur.jpg",blurfilter)
+    case 3:
+        cv2.imwrite("outputpencilsketch.jpg",pencilsketch)
+    case 4:
+        print("Hasta Luego")
+            
