e5f001df8c199779ddd51dab3e716431b5cbc39a
Demian <53890358+demian35@users.noreply.github.com>
2024-03-28 23:59:36 -0600

    funcion para aplicar filtros a la foto

--- convierteImagen.py
+++ convierteImagen.py
@@ -13,37 +13,25 @@ def cargaImagen():
         return None
 
 
-imagen= cv2.imread("IMG_3110.JPG")
-#le pasamos la imagen con la que trabajaremos
-
-#aplicamos filtro a la foto , en ese caso usamos la funcion cvtcolor y le pasamos el color gris de cv2
-filtrogrises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
-#invertimos el flitro de grises
-invert=cv2.bitwise_not(filtrogrises)
-
-#aplicamos el fitro blur al invert
-blurfilter=cv2.GaussianBlur(invert,(21,21),0)
-
-#invertimos a blur
-invertblur=cv2.bitwise_not(blurfilter)
-
-#aplicamos el pencil sketch
-pencilsketch=cv2.divide(filtrogrises,invertblur,scale=256.0)
-
-
-
-
-print("Que filtro desea usar : seleccione una opcion 1 grises , 2 blur , 3 pencilsketch , 4 salir")
-opcion=int(input())
-#regresamos la imagen pintada a lapiz
-
-match opcion:
-    case 1:
-        cv2.imwrite("outputgrey.jpg", filtrogrises)
-    case 2:
-        cv2.imwrite("outputblur.jpg",blurfilter)
-    case 3:
-        cv2.imwrite("outputpencilsketch.jpg",pencilsketch)
-    case 4:
-        print("Hasta Luego")
+def aplicaFiltro(opcion):
+    imagen=cargaImagen()
+    if imagen is not None:
+        #aplicamos filtro a la foto , en ese caso usamos la funcion cvtcolor y le pasamos el color gris de cv2
+        filtrogrises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+        #invertimos el flitro de grises
+        invert=cv2.bitwise_not(filtrogrises)
+        #aplicamos el fitro blur al invert
+        blurfilter=cv2.GaussianBlur(invert,(21,21),0)
+        #invertimos a blur
+        invertblur=cv2.bitwise_not(blurfilter)
+        #aplicamos el pencil sketch
+        pencilsketch=cv2.divide(filtrogrises,invertblur,scale=256.0)
+        if opcion==1:
+            cv2.imwrite("outputgrey.jpg", filtrogrises)
+        elif opcion==2:
+            cv2.imwrite("outputblur.jpg",blurfilter)
+        elif opcion==3:
+            cv2.imwrite("outputpencilsketch.jpg",pencilsketch)
+        else:
+            print("Opcion no valida")
             
